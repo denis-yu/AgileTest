55ba86fca3abd131c05131a880c09ba20b6aefd5
denis-yu <denis.yu@suryani.cn>
2018-12-13 16:10:03 +0800

    new develop branch

--- Test Suites/Regression/Level 1.ts
+++ Test Suites/Regression/Level 1.ts
@@ -4,23 +4,50 @@
    <name>Level 1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-12-11T15:43:53</lastRun>
-   <mailRecipient></mailRecipient>
+   <lastRun>2018-12-13T15:41:16</lastRun>
+   <mailRecipient>denis.yu@suryani.cn;</mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
-   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
    <testSuiteGuid>3724ef24-0d33-4994-b5a1-93157467c1e7</testSuiteGuid>
    <testCaseLink>
       <guid>86bfcfff-0723-4716-9152-b872f811b1e1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>false</isRun>
-      <testCaseId>Test Cases/Regression/Level 1/LifeShield</testCaseId>
+      <testCaseId>Test Cases/Regression/Level 1/Everest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>7b4bfe91-f7b2-44d3-a981-d39af81a34f9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Regression/Level 1/LifeShield - Copy</testCaseId>
+      <testCaseId>Test Cases/Regression/Level 1/LifeShield</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>120e0c3b-6d67-410a-a209-df5beabf31fb</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Level 1/Lifeshield</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>120e0c3b-6d67-410a-a209-df5beabf31fb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>﻿zipcode</value>
+         <variableId>05673849-07a0-4ae5-8079-7ccc8e37c53b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>120e0c3b-6d67-410a-a209-df5beabf31fb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>effectiveDate</value>
+         <variableId>51962a78-58b1-42d5-8657-f00b08fce272</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>120e0c3b-6d67-410a-a209-df5beabf31fb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>dob</value>
+         <variableId>3e6e20b3-bf4d-4137-b457-f3b34df976f6</variableId>
+      </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
