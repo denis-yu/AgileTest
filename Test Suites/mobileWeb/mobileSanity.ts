4ed7f2a14151f7f25679b6592aecff298c6aecc2
denis-yu <denis.yu@suryani.cn>
2019-02-13 15:58:17 +0800

    Merge branch 'Develop' into windows
    * Develop:
      slaico2
      Lifeshield
      slaico update
      slaico success case
    
    # Conflicts:
    #	Scripts/mobileWeb/stm/Slaico/Script1546076568501.groovy
    #	Test Suites/mobileWeb/mobileSanity.ts
    #	appium.log
    #	settings/external/com.kms.katalon.core.db.DatabaseSettings.properties
    #	settings/internal/com.kms.katalon.core.mobile.android.properties
    #	settings/internal/com.kms.katalon.core.webservice.properties
    #	settings/internal/com.kms.katalon.execution.properties
    #	settings/internal/com.kms.katalon.execution.webui.properties
    #	settings/internal/com.kms.katalon.integration.analytics.properties

--- Test Suites/mobileWeb/mobileSanity.ts
+++ Test Suites/mobileWeb/mobileSanity.ts
@@ -58,67 +58,37 @@
    <testCaseLink>
       <guid>46a7ddf9-c889-4dac-bbd6-51ddd9b54b37</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/mobileWeb/Dental/USA Access</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>39eff333-e2a2-442a-aadb-9f343cae9e61</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/mobileWeb/Dental/National General</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>de8a34cb-bc91-4bc4-9098-f1e20014c190</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/mobileWeb/stm/Slaico</testCaseId>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>7141a177-36a3-434f-97c0-6810e05d4317</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>be9c6e43-d249-4caf-8db2-3d551811e7dd</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>e6ec1771-b963-4611-82dc-9dac6b1a0061</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>ec0323b0-bf4e-44f7-a24f-d7f346c4dc4d</variableId>
-      </variableLink>
    </testCaseLink>
    <testCaseLink>
       <guid>1631faf3-329f-4cbc-a5dd-b50513c7b713</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/mobileWeb/HBI/Core Shield</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>fc144e1e-987f-4cd0-b319-8bc07c82c01a</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/mobileWeb/HBI/Cardinal Choice</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>bd5807a0-0d9a-4bbc-a2d1-9d37a427ec07</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/mobileWeb/HBI/Agile One</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>aa0e8087-ec06-4d4b-b8fc-08713fdd828a</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/mobileWeb/HBI/Axis Legion</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
